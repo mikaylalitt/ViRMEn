52bc548618ad48badfbb665db092c858d0331627
Mikayla Litt <30472726+m-litt@users.noreply.github.com>
2018-06-06 12:30:14 -0500

    Update reward.m
    Changed to solely give a reward. All other conditions to be met to give a reward must be check prior to calling this function.

--- reward.m
+++ reward.m
@@ -1,15 +1,23 @@
-% function reward(vr,rewardTime)
-% queueOutputData(vr.waterSession,[ones(1,rewardTime)*vr.highVoltage vr.lowVoltage]'); 
-% % vr.waterSession is the data acquisition session
-% % "ones(1,rewardTime)" returns a 1 by 1 array of ones of data type rewardTime
-% startBackground(vr.waterSession);
-
-% ML commented out above and added below based off of:
-% "alternateEndZoneReward.m"
-
 function vr = reward(vr)
+% delivers a reward and increments the number of rewards delivered counter variable
 
-% ML 
+disp('REWARD');
+
+% deliver the reward:
+for drop = 1:vr.dropsToSend
+    queueOutputData(vr.waterSession,[ones(1,vr.timeSolenoid)*vr.highVoltage vr.lowVoltage]');
+    startBackground(vr.waterSession);
+end
+
+% count the reward:
+vr.numRewards = vr.numRewards + 1;
+
+
+
+
+% ------------------------------------------------------- above is newly edited code, below is previous code
+
+
 if ((vr.position(1) < -23) && (vr.position(2) > 23)) % mouse is in left reward zone
 %     reward(vr,vr.timeSolenoid);
     if ((vr.prevY_Posn < 23 || vr.prevX_Posn > -23) && strcmp(vr.prevHall,'main'))
