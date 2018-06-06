9ce80859da42bcae37d7b53982b58bf284c3dd54
Mikayla Litt <30472726+m-litt@users.noreply.github.com>
2018-06-06 12:25:17 -0500

    Update alternateEndZoneReward.m
    Cleaned up the code and added comments

--- alternateEndZoneReward.m
+++ alternateEndZoneReward.m
@@ -1,50 +1,42 @@
 function vr = alternateEndZoneReward(vr)
 
-queueOutputData(vr.waterSession,[ones(1,vr.timeSolenoid)*vr.highVoltage vr.lowVoltage]');
-startBackground(vr.waterSession);
-    
-% update the previous endzone visited
-vr.lastEndZoneRewarded = mod(vr.lastEndZoneRewarded,2) + 1;
-    
-% count the reward
-vr.numRewards = vr.numRewards + 1;
-    
-disp('REWARD');
-vr.gaveFirstReward = true;
+% locations of the endzone boundaries
+% (these should be updated based on the locations of the endzones of the track being used):
+vr.endZoneLoc1 = str2double(vr.exper.variables.arenaLength)-str2double(vr.exper.variables.endZoneLength);
+vr.endZoneLoc2 = str2double(vr.exper.variables.endZoneLength);
 
-% % if the mouse is in the opposite endzone from before
-% if (((vr.position(2) > str2double(vr.exper.variables.arenaLength)-str2double(vr.exper.variables.endZoneLength)) ...
-%         && (lastEndZoneRewarded == 1 || ~vr.gaveFirstReward))...
-%         || (vr.position(2) < str2double(vr.exper.variables.endZoneLength) ...
-%         && (vr.lastEndZoneRewarded == 2 || ~vr.gaveFirstReward)))
-%     
-%     % deliver the reward
-% %     try
-% %         for drop=1:vr.dropsToSend
-% % %             queueOutputData(vr.waterSession,[ones(1,vr.timeSolenoid)*vr.highVoltage vr.lowVoltage]');
-% % %             startBackground(vr.waterSession);
-% %             vr.waterSession.queueOutputData([ones(1,vr.timeSolenoid)*vr.highVoltage vr.lowVoltage]');
-% %             vr.lh = vr.waterSession.addlistener('DataAvailable',@(src,event) test2(src, event));
-% %             vr.waterSession.startBackground
-% %             lh.Enabled = false;
-% %             delete(lh);
-% %             vr.waterSession.wait();
-% %         end
-% %     catch err
-% %         queueOutputData(vr.waterSession,[ones(1,vr.timeSolenoid)*vr.highVoltage vr.lowVoltage]');
-% %         startBackground(vr.waterSession);
-% %     end
-% 
-%    % deliver the reward
-%     queueOutputData(vr.waterSession,[ones(1,vr.timeSolenoid)*vr.highVoltage vr.lowVoltage]');
-%     startBackground(vr.waterSession);
-%     
-%     % update the previous endzone visited
-%     vr.lastEndZoneRewarded = mod(vr.lastEndZoneRewarded,2) + 1;
-%     
-%     % count the reward
-%     vr.numRewards = vr.numRewards + 1;
-%     
-%     disp('REWARD');
-%     vr.gaveFirstReward = true;
-% end
+% if the mouse is in the opposite endzone as before, deliver a reward 
+% (for the linear track only the y-position of the mouse needs to be compared):
+if (vr.position(2) > vr.endZoneLoc1) && (vr.lastEndZoneRewarded == 1 || ~vr.gaveFirstReward)
+    
+    if ~vr.gaveFirstReward % if this is the first reward
+       vr.gaveFirstReward = true;
+    end
+    
+    vr.lastEndZoneRewarded = 2; % update the last endzone that was rewarded to be this endzone
+    vr = reward(vr); % deliver the reward
+    
+elseif (vr.position(2) < vr.endZoneLoc2) && (vr.lastEndZoneRewarded == 2 || ~vr.gaveFirstReward)
+    
+    if ~vr.gaveFirstReward
+       vr.gaveFirstReward = true;
+    end
+    
+    vr.lastEndZoneRewarded = 1; 
+    vr = reward(vr);
+    
+end
+
+% ---------------------------------------------------------- above is the newly edited code and below is the old code
+
+% queueOutputData(vr.waterSession,[ones(1,vr.timeSolenoid)*vr.highVoltage vr.lowVoltage]');
+% startBackground(vr.waterSession);
+    
+% % update the previous endzone visited
+% vr.lastEndZoneRewarded = mod(vr.lastEndZoneRewarded,2) + 1;
+    
+% % count the reward
+% vr.numRewards = vr.numRewards + 1;
+    
+% disp('REWARD');
+% vr.gaveFirstReward = true;
